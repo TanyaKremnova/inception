c104c59b0b761abd636223553f70cc66547ea1b2
Tanya Kremnova <kremnova.tanya@gmail.com>
2026-06-17 11:27:24 +0200

    small changes in mariadb init.sh

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -7,7 +7,9 @@ chown -R mysql:mysql /run/mysqld
 DB_PASSWORD=${DB_PASSWORD}
 DB_ROOT_PASSWORD=${DB_ROOT_PASSWORD}
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+INIT_MARKER="/var/lib/mysql/.inception_initialized"
+
+if [ ! -f "${INIT_MARKER}" ]; then
 
     echo ">>> Initializing MariaDB data directory..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
@@ -23,6 +25,8 @@ CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_ADMIN_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+
+    touch "${INIT_MARKER}"
     echo ">>> Bootstrap done."
 
 fi
