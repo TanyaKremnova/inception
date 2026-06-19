39a154ca4464f13be4cac4efad80e446b2d437c2
Tanya Kremnova <kremnova.tanya@gmail.com>
2026-06-19 14:46:23 +0200

    test 01 changes

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -15,6 +15,10 @@ if [ ! -f "${CERT}" ]; then
         -out "${CERT}" \
         -days 365 \
         -subj "/C=NL/ST=Noord-Holland/L=Amsterdam/O=42/CN=${DOMAIN_NAME}"
+
+    sed -e "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" \
+        -e "s/\${WP_PORT}/${WP_PORT}/g" \
+        /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
 # Start nginx in foreground as PID 1
